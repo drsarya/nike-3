ae2ba36c6f0d53fb3a83d9814cfb4450eaaffd60
Darya <drsarya043@gmail.com>
2024-01-23 13:55:06 -0800

    Initial Commit

---
+++ nike3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nike3
+//
+//  Created by darya on 23.01.2024.
+//  Copyright © 2024 darya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
