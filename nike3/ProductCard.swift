6245c268a0eb5f3b4a0cea015ee640bc453651e8
Darya <drsarya043@gmail.com>
2024-01-24 13:38:07 -0800

    r

--- nike3/ProductCard.swift
+++ nike3/ProductCard.swift
@@ -36,12 +36,12 @@ struct ProductCard: View {
         
         return  ScrollView(.vertical) {
             ScrollView(.horizontal) {
-                            HStack {
-                                Text("First Row")
-                                Text("Second Row")
-                                Text("Third Row")
-                            }
-                        }
+                HStack {
+                    Text("First Row")
+                    Text("Second Row")
+                    Text("Third Row")
+                }
+            }
             ForEach(0..<images.count, id: \.self) { array in
                 HStack{
                     ForEach(images[array], id: \.self) {                 value in ProductCardView(imageData: value)
@@ -51,8 +51,8 @@ struct ProductCard: View {
         }
     }
 }
-    
- 
+
+
 
 
 struct ProductCard_Previews: PreviewProvider {
@@ -65,24 +65,50 @@ struct ProductCardView: View {
     var imageData: ProductData;
     var body: some View {
         VStack() {
-            Image(imageData.image)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 178, height:185)
-                .clipped()
-                .padding(.bottom, 10)
+            ZStack {
+                Image(imageData.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 178, height:185)
+                    .clipped()
+                    .padding(.bottom, 10)
+                LikeButton()
+                    .frame(maxWidth: 170, maxHeight:178 , alignment: .topTrailing)
+            }
+            
             if(imageData.isSoldOut){
                 Text("Sold Out")
                     .font(Font.custom("Inter", size: 14).weight(.semibold))
-                      .foregroundColor(Color(red: 0.94, green: 0.47, blue: 0.32))
-
+                    .foregroundColor(Color(red: 0.937, green: 0.475, blue: 0.322))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
             }
             Text(imageData.name)
+                .frame(maxWidth: .infinity, alignment: .leading)
             Text(imageData.description)
+                .foregroundColor(Color(red: 0.384, green: 0.361, blue: 0.361))
+                .frame(maxWidth: .infinity, alignment: .leading)
             Text(imageData.colors)
+                .frame(maxWidth: .infinity, alignment: .leading)
             Text(imageData.price)
                 .font(Font.custom("Inter", size: 14).weight(.bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
         }.frame(width: 180, height: 310, alignment: .topLeading)
-            
+        
+    }
+}
+
+struct LikeButton: View {
+    
+    var body: some View {
+        Button (action: {}) {
+            ZStack {
+                Ellipse()
+                    .foregroundColor(.white)
+                    .frame(width: 28, height: 28)
+                Image("like")
+                    .foregroundColor(.black)
+            }
+        }
     }
 }
